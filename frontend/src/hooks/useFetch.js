d91142ba846d017f2a3a0090898ad25b3536e689
rnogara <r.nogara.dev@gmail.com>
2023-08-08 18:31:19 -0300

    Change URLS at hooks/useRecipes and change fetch method at hooks/useFetch

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,21 +1,24 @@
 import { useState } from 'react';
+import api from '../utils/api';
 
 function useFetch(initialDataValue = null) {
   const [isLoading, setIsLoading] = useState(false);
   const [errors, setErrors] = useState(null);
   const [data, setData] = useState(initialDataValue);
   const fetchingData = async (URL) => {
-    try {
-      setIsLoading(true);
-      const response = await fetch(URL);
-      // if (???) {
-      // verify false return of API
-      // const error = await response.json();
-      // throw error.message;
-      // }
-      const dataFetched = await response.json();
-      setData(dataFetched);
-    } catch (error) { setErrors(error); } finally { setIsLoading(false); }
+    api.get(URL).then((response) => setData(response)).catch((e) => setErrors(e))
+      .finally(setIsLoading(false));
+    // try {
+    //   setIsLoading(true);
+    //   const response = await fetch(URL);
+    //   // if (???) {
+    //   // verify false return of API
+    //   // const error = await response.json();
+    //   // throw error.message;
+    //   // }
+    //   const dataFetched = await response.json();
+    //   setData(dataFetched);
+    // } catch (error) { setErrors(error); } finally { setIsLoading(false); }
   };
   return [isLoading, errors, data, fetchingData];
 }
