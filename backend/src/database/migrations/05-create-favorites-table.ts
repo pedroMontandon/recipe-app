6c3866eb1f50b323be28c6cd48ed41de8539aa0b
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-08-09 10:23:56 -0300

    Cria a migration da tabela de favorites

---
+++ backend/src/database/migrations/05-create-favorites-table.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Model, QueryInterface } from 'sequelize';
+import IFavorite from '../../interfaces/favorites/IFavorite';
+
+export default {
+  up: async (queryInterface: QueryInterface) => {
+    await queryInterface.createTable<Model<IFavorite>>(
+      'favorites',
+      {
+        userId: {
+          type: DataTypes.INTEGER.UNSIGNED,
+          allowNull: false,
+          primaryKey: true,
+          field: 'user_id',
+          references: {
+            model: 'user',
+            key: 'id',
+          },
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE',
+        },
+        recipeId: {
+          type: DataTypes.INTEGER.UNSIGNED,
+          allowNull: false,
+          primaryKey: true,
+          field: 'recipe_id',
+          references: {
+            model: 'recipes',
+            key: 'id',
+          },
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE',
+        }
+      },
+    );
+  },
+  down: async (queryInterface: QueryInterface) => {
+    await queryInterface.dropTable('favorites');
+  },
+};
