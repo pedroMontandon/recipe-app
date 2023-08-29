5c6b6389449e5b16cf8d9ff4fcf2f06050fc0d53
pedroMontandon <pedrobarbosa89@gmail.com>
2023-08-29 17:03:50 -0300

    resolvendo a safadeza

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -1,19 +1,12 @@
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'allexpress_admin',
-  password: process.env.MYSQL_ROOT_PASSWORD || 'OvPie7fOi8A8jbCcJZdW',
+  username: process.env.DB_USER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
   database: process.env.DB_NAME || 'recipes_db',
-  host: process.env.DB_HOST || 'recipe-app-db.cmco3eo7uytp.sa-east-1.rds.amazonaws.com',
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
-  // dialectOptions: {
-  //   type: 'host',
-  //   database: process.env.DB_NAME || 'recipes_db',
-  //   user: process.env.DB_USER || 'allexpress_admin',
-  //   address: process.env.DB_HOST || 'recipe-app-db.cmco3eo7uytp.sa-east-1.rds.amazonaws.com',
-  //   method: 'password',
-  // },
   logging: true,
 };
 
