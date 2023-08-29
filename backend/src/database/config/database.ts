61a04e2cc9a5ed7adf8f423678500edb085462a5
pedroMontandon <pedrobarbosa89@gmail.com>
2023-08-29 16:53:26 -0300

    safadeza, não mergear

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -1,15 +1,19 @@
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.MYSQL_ROOT_PASSWORD || 'root',
+  username: process.env.DB_USER || 'allexpress_admin',
+  password: process.env.MYSQL_ROOT_PASSWORD || 'OvPie7fOi8A8jbCcJZdW',
   database: process.env.DB_NAME || 'recipes_db',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'recipe-app-db.cmco3eo7uytp.sa-east-1.rds.amazonaws.com',
   port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
+  // dialectOptions: {
+  //   type: 'host',
+  //   database: process.env.DB_NAME || 'recipes_db',
+  //   user: process.env.DB_USER || 'allexpress_admin',
+  //   address: process.env.DB_HOST || 'recipe-app-db.cmco3eo7uytp.sa-east-1.rds.amazonaws.com',
+  //   method: 'password',
+  // },
   logging: true,
 };
 
