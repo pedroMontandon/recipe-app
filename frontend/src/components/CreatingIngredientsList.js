cffbecd24af959f2c1b17754625edb33b901eccf
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-10-13 23:52:55 -0300

    feat: Adds the option to delete a ingredient added to the recipe

--- frontend/src/components/CreatingIngredientsList.js
+++ frontend/src/components/CreatingIngredientsList.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function CreatingIngredientsList({ ingredients, ingredientNames }) {
+function CreatingIngredientsList({ ingredients, ingredientNames, func }) {
+  const deleteIngredient = (indexToRemove) => {
+    const newValue = ingredients.filter((_, index) => index !== indexToRemove);
+    func({ target: { name: 'ingredients', value: newValue } });
+  };
+
   return (
     <ol>
       {
-        ingredients.map((ingredient) => (
+        ingredients.map((ingredient, index) => (
           <li key={ `${ingredient.id}-ingredient-in-recipe` }>
-            { ingredient.measure }
-            { ' ' }
-            { ingredientNames[ingredient.id - 1].name }
+            <span>
+              { ingredient.measure }
+              { ' ' }
+              { ingredientNames[ingredient.id - 1].name }
+            </span>
+            <button type="button" onClick={ () => deleteIngredient(index) }>
+              Delete ingredient
+            </button>
           </li>
         ))
       }
@@ -26,6 +36,7 @@ CreatingIngredientsList.propTypes = {
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
   })).isRequired,
+  func: PropTypes.func.isRequired,
 };
 
 export default CreatingIngredientsList;
